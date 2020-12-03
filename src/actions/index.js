ac40685a83bd3596b9b34246c10dff5c07c4feb8
unknown <mounikareddygonae15@gmail.com>
2020-12-03 13:15:45 +0530

    add store

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+
+
 {
     type: 'ADD_MOVIES'
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import { act } from "react-dom/test-utils";
+
 
 export default function movies(state=[], action) {
     if(action.type === 'ADD_MOVIES') {
